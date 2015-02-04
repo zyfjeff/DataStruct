a28bee41b31334ad4499406fae7537c2fb235adc
zyfjeff <445188383@qq.com>
2015-02-04 01:57:58 +0000

    链表实现的栈

---
+++ linkstack.h
@@ -0,0 +1,18 @@
+#ifndef STACK_INCLUDE
+#define STACK_INCLUDE
+
+/*
+ *	简化声明方式
+ */
+
+#define T Stack_T
+typedef struct T *T;
+
+extern T Stack_new(void);
+extern int Stack_empty(T stk);
+extern void Stack_push(T stk,void *x);
+extern void *Stack_pop(T stk);
+extern void Stack_free(T *stk);
+
+#undef T
+#endif
